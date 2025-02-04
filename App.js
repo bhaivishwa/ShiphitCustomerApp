ebfad76d3f93469cb71cd38d36375309b46bb862
bhaivishwa <bhaivishwa338@gmail.com>
2025-02-03 22:50:48 -0800

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Happy coding</Text>
+      <Text>Happy hello </Text>
     </View>
   );
 }
